2cf57e9a9bf2d56de456add83e5e8003469735f5
okoeee <yk27.puxcm@gmail.com>
2025-05-04 15:57:54 +0900

    remove createdAt from user model

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -1,13 +1,7 @@
 package models
 
-import (
-	"time"
-)
-
 type User struct {
-	UserID    string
-	Name      string
-	Email     string
-	CreatedAt time.Time 
-	UpdatedAt time.Time 
+	UserId string
+	Name   string
+	Email  string
 }
