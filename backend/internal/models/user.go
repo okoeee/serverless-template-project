c3f9987285822a4e246966acd22b739ef9e67f25
okoeee <yk27.puxcm@gmail.com>
2025-05-05 14:56:32 +0900

    define userid with uuid

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -1,7 +1,36 @@
 package models
 
+import "github.com/google/uuid"
+
+type UserId uuid.UUID
+
+func newUserId() UserId {
+	return UserId(uuid.New())
+}
+
+func (id UserId) String() string {
+	return uuid.UUID(id).String()
+}
+
+func ParseUserId(id string) (UserId, error) {
+	userId, err := uuid.Parse(id)
+	if err != nil {
+		return UserId{}, err
+	}
+	return UserId(userId), nil
+}
+
 type User struct {
-	UserId string
+	UserId UserId
 	Name   string
 	Email  string
+}
+
+func NewUser(name, email string) *User {
+	user := User{
+		UserId: newUserId(),
+		Name:   name,
+		Email:  email,
+	}
+	return &user
 }
