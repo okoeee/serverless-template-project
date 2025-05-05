74212517b6877cc0dfd3f4cd88bc249c839b45a2
okoeee <yk27.puxcm@gmail.com>
2025-05-05 22:43:14 +0900

    implement ping pong

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"context"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-
-	"backend/internal/handlers"
+	"github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-gonic/gin"
 )
 
+var ginLambda *ginadapter.GinLambdaV2
+
+func init() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	ginLambda = ginadapter.NewV2(r)
+}
+
+func Handler(ctx context.Context, request events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	return ginLambda.ProxyWithContext(ctx, request)
+}
+
 func main() {
-	lambda.Start(handlers.SampleRequestHandler)
+	lambda.Start(Handler)
 }
